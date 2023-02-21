fafc17d2d0e6f8b006f9b53e6d16817c4a34c0ad
Aishwarya S R <Aishwarya.SR>
2023-02-21 17:49:22 +0530

    test: Test for error in BlogPosts c omponent

--- src/components/BlogPosts/__tests__/index.test.tsx
+++ src/components/BlogPosts/__tests__/index.test.tsx
@@ -2,17 +2,30 @@ import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import { mockBlogPostData } from "../../../mocks/blogPosts";
 import BlogPosts from "..";
+import makeRequest from "../../../utils/makeRequest";
 
-jest.mock("../../../utils/makeRequest/", () => () => {
-  return Promise.resolve(mockBlogPostData);
-});
+jest.mock("../../../utils/makeRequest/");
 
 describe("BlogPosts", () => {
+  const mockedMakeRequest = makeRequest as jest.MockedFunction<
+    typeof makeRequest
+  >;
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it("should render correctly", async () => {
+    mockedMakeRequest.mockResolvedValueOnce(mockBlogPostData);
     const { asFragment } = render(<BlogPosts />);
     await waitFor(() => {
       expect(screen.getByText("mock title 1")).toBeTruthy();
     });
     expect(asFragment()).toMatchSnapshot();
+  });
+  it("should show error message when GET /blog-data API returns error", async () => {
+    mockedMakeRequest.mockRejectedValueOnce({ message: "Error!" });
+    render(<BlogPosts />);
+    await waitFor(() => {
+      expect(screen.getByText("Error!")).toBeTruthy();
+    });
   });
 });
