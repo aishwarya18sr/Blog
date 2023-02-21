cce3b554c9c4f114619c0258355237ef5f9102e8
Aishwarya S R <Aishwarya.SR>
2023-02-21 11:34:00 +0530

    chore(blog-posts): update test cases

--- src/components/BlogPosts/__tests__/index.test.tsx
+++ src/components/BlogPosts/__tests__/index.test.tsx
@@ -1,10 +1,4 @@
-import {
-  act,
-  render,
-  renderHook,
-  screen,
-  waitFor,
-} from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import { mockBlogPostData } from "../../../mocks/blogPosts";
 import BlogPosts from "..";
