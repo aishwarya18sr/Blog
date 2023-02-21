1cc1e81d070cff57a594c110a203db8702fa8d72
Aishwarya S R <Aishwarya.SR>
2023-02-21 11:33:07 +0530

    chore(blog-posts): update test cases

--- src/components/BlogPosts/__tests__/index.test.tsx
+++ src/components/BlogPosts/__tests__/index.test.tsx
@@ -1,15 +1,24 @@
-import { render } from "@testing-library/react";
+import {
+  act,
+  render,
+  renderHook,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import React from "react";
 import { mockBlogPostData } from "../../../mocks/blogPosts";
-import BlogPosts from "../../BlogPosts";
+import BlogPosts from "..";
 
 jest.mock("../../../utils/makeReuqest/", () => () => {
   return Promise.resolve(mockBlogPostData);
 });
 
 describe("BlogPosts", () => {
-  it("should render correctly", () => {
+  it("should render correctly", async () => {
     const { asFragment } = render(<BlogPosts />);
+    await waitFor(() => {
+      expect(screen.getByText("mock title 1")).toBeTruthy();
+    });
     expect(asFragment()).toMatchSnapshot();
   });
 });
