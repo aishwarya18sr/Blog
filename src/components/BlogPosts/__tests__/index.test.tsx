6dbb69744d571dfb5eddba96920c916df6f94ac3
Aishwarya S R <Aishwarya.SR>
2023-02-22 12:58:39 +0530

    chore: update unit test cases

--- src/components/BlogPosts/__tests__/index.test.tsx
+++ src/components/BlogPosts/__tests__/index.test.tsx
@@ -1,31 +1,41 @@
-import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import { mockBlogPostData } from "../../../mocks/blogPosts";
+import React from "react";
 import BlogPosts from "..";
+import { mockBlogPostData } from "../../../mocks/blogPosts";
 import makeRequest from "../../../utils/makeRequest";
 
-jest.mock("../../../utils/makeRequest/");
+jest.mock("../../../utils/makeRequest");
 
 describe("BlogPosts", () => {
-  const mockedMakeRequest = makeRequest as jest.MockedFunction<
-    typeof makeRequest
-  >;
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-  it("should render correctly", async () => {
-    mockedMakeRequest.mockResolvedValueOnce(mockBlogPostData);
-    const { asFragment } = render(<BlogPosts />);
+  it("should show loading text when data is still loading", async () => {
+    const mockMakeRequest = makeRequest as jest.MockedFunction<
+      typeof makeRequest
+    >;
+    mockMakeRequest.mockResolvedValue(mockBlogPostData);
+    render(<BlogPosts />);
+    expect(screen.getByText("Loading...")).toBeTruthy();
     await waitFor(() => {
       expect(screen.getByText("mock title 1")).toBeTruthy();
     });
-    expect(asFragment()).toMatchSnapshot();
   });
-  it("should show error message when GET /blog-data API returns error", async () => {
-    mockedMakeRequest.mockRejectedValueOnce({ message: "Error!" });
+  it("should show the blog posts when data is loaded", async () => {
+    const mockMakeRequest = makeRequest as jest.MockedFunction<
+      typeof makeRequest
+    >;
+    mockMakeRequest.mockResolvedValue(mockBlogPostData);
     render(<BlogPosts />);
     await waitFor(() => {
-      expect(screen.getByText("Error!")).toBeTruthy();
+      expect(screen.getAllByTestId("blog-post")).toHaveLength(2);
+    });
+  });
+  it("should show error message when there is error in data fetch", async () => {
+    const mockMakeRequest = makeRequest as jest.MockedFunction<
+      typeof makeRequest
+    >;
+    mockMakeRequest.mockRejectedValue({ message: "Error!!!!" });
+    render(<BlogPosts />);
+    await waitFor(() => {
+      expect(screen.getByText("Error!!!!")).toBeTruthy();
     });
   });
 });
--- src/utils/makeRequest/__tests__/index.test.ts
+++ src/utils/makeRequest/__tests__/index.test.ts
@@ -1,48 +1,48 @@
+import { mockBlogPostData } from "./../../../mocks/blogPosts";
+import axios from "axios";
+import makeRequest from "..";
 import {
   BACKEND_URL,
+  GET_BLOG_DATA,
   UPDATE_BLOG_DATA,
-} from "./../../../constants/apiEndPoints";
-import axios from "axios";
-import { mockBlogPostData } from "../../../mocks/blogPosts";
-import makeRequest from "..";
-import { GET_BLOG_DATA } from "../../../constants/apiEndPoints";
+} from "../../../constants/apiEndPoints";
 
 jest.mock("axios");
 
 describe("makeRequest", () => {
-  const mockedAxios = axios as jest.MockedFunction<typeof axios>;
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-  it("should make API call with appropriate request options and return response body when only endpoint is specified", async () => {
-    mockedAxios.mockResolvedValueOnce({
-      data: mockBlogPostData,
-    });
-    expect(mockedAxios).not.toHaveBeenCalled();
+  it("should make api call with appropriate request options and return response body when only api endpoint is specified", async () => {
+    const mockedAxios = axios as jest.MockedFunction<typeof axios>;
+    mockedAxios.mockResolvedValue({ data: mockBlogPostData });
+    expect(mockedAxios).not.toBeCalled();
     const response = await makeRequest(GET_BLOG_DATA);
     expect(mockedAxios).toHaveBeenCalledTimes(1);
     expect(mockedAxios).toHaveBeenCalledWith({
       baseURL: BACKEND_URL,
-      url: GET_BLOG_DATA.url,
+      url: "blog-posts",
       method: "get",
     });
     expect(response).toEqual(mockBlogPostData);
   });
-  it("should make API call with appropriate request options and return response body when both endpoint and request body are specified", async () => {
-    mockedAxios.mockResolvedValueOnce({
-      data: { claps: 1 },
-    });
-    expect(mockedAxios).not.toHaveBeenCalled();
+  it("should make api call with appropriate request options and return response body when api endpoint and request body is specified", async () => {
+    const mockedAxios = axios as jest.MockedFunction<typeof axios>;
+    mockedAxios.mockResolvedValue({ data: { data: { claps: 1 } } });
+    expect(mockedAxios).not.toBeCalled();
     const response = await makeRequest(UPDATE_BLOG_DATA(1), {
       data: { claps: 1 },
     });
     expect(mockedAxios).toHaveBeenCalledTimes(1);
     expect(mockedAxios).toHaveBeenCalledWith({
       baseURL: BACKEND_URL,
-      url: UPDATE_BLOG_DATA(1).url,
+      url: "blog-posts/1",
       method: "put",
-      data: { claps: 1 },
+      data: {
+        claps: 1,
+      },
     });
-    expect(response).toEqual({ claps: 1 });
+    expect(response).toEqual({
+      data: {
+        claps: 1,
+      },
+    });
   });
 });
