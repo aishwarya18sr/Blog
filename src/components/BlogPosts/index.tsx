2c1afd97fcb31e66549c347f3969a156c9fb0db6
Aishwarya S R <Aishwarya.SR>
2023-02-22 10:53:47 +0530

    chore: add state upliftment

--- src/components/BlogPosts/index.tsx
+++ src/components/BlogPosts/index.tsx
@@ -2,25 +2,42 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { GET_BLOG_DATA } from "../../constants/apiEndPoints";
 import { BlogData } from "../../types";
+import { getBlogIndexById } from "../../utils/common";
 import makeRequest from "../../utils/makeRequest";
 import BlogPostCard from "../BlogPostCard";
 import "./blogPosts.css";
 
 const BlogPosts = () => {
-  const [blogData, setBlogData] = useState<BlogData[]>();
+  const [allBlogData, setAllBlogData] = useState<BlogData[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     makeRequest(GET_BLOG_DATA, {}, navigate).then((response) => {
-      setBlogData(response);
+      setAllBlogData(response);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return blogData ? (
+  return allBlogData ? (
     <div className="posts basic-padding">
-      {blogData.map((eachBlogData) => {
-        return <BlogPostCard key={eachBlogData.id} blogData={eachBlogData} />;
+      {allBlogData.map((eachBlogData) => {
+        return (
+          <BlogPostCard
+            key={eachBlogData.id}
+            blogData={eachBlogData}
+            setAllBlogData={(updatedBlogData: BlogData) => {
+              const blogDataIndex = getBlogIndexById(
+                allBlogData,
+                updatedBlogData.id
+              );
+              setAllBlogData([
+                ...allBlogData.slice(0, blogDataIndex),
+                updatedBlogData,
+                ...allBlogData.slice(blogDataIndex + 1),
+              ]);
+            }}
+          />
+        );
       })}
     </div>
   ) : (
